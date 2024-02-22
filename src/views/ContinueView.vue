94b137e150518ae619e0b3d3dc0356b4994e1579
Rahmi Hatipoglu <rahmihatipoglu@hotmail.com>
2024-02-22 13:50:39 +0300

    continue sayfasında modal kullanıldı

--- src/views/ContinueView.vue
+++ src/views/ContinueView.vue
@@ -1,7 +1,36 @@
+<script setup>
+import router from '@/router/index.js'
+
+function sayfayaGonder() {
+  router.push({ name: "home" }) // Başka sayfaya gönderme
+}
+
+
+</script>
 <template>
   <div class="container">
     <h4>This is an continue page</h4>
+    <dialog open>
+      <article>
+        <h2>İşleminiz Başarılı</h2>
+        <p>
+          <slot></slot>
+        </p>
+        <!-- <ul>
+          İşleminiz başarıyla tamamlandı.
+          <li>Membership: Individual</li>
+          <li>Price: $10</li>
+        </ul> -->
+        <footer>
+          <button className="secondary">
+            İptal
+          </button>
+          <button @click="sayfayaGonder()">Tamam
 
+          </button>
+        </footer>
+      </article>
+    </dialog>
   </div>
 </template>
 
